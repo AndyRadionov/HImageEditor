50c7c3f788df7cab7a1d647a6f2ca221d5d9aa74
Andrey Radionov <andyomsk@gmail.com>
2018-12-28 18:06:03 +0600

    HistoryHelper

---
+++ app/src/main/java/io/github/andyradionov/himageeditor/utils/HistoryHelper.kt
@@ -0,0 +1,36 @@
+package io.github.andyradionov.himageeditor.utils
+
+import android.content.Context
+import android.preference.PreferenceManager
+import io.github.andyradionov.himageeditor.R
+import java.util.*
+
+/**
+ * @author Andrey Radionov
+ */
+object HistoryHelper {
+    private const val HISTORY_LIST_SIZE = 100
+
+    fun loadHistory(context: Context): MutableList<String> {
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context)
+        val defSet = HashSet<String>()
+        val history = sharedPrefs.getStringSet(context.getString(R.string.history_key), defSet)
+        val result = ArrayList(history!!)
+        result.sortDescending()
+        return result
+    }
+
+    fun updateHistoryList(context: Context, imgPath: String) {
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context)
+
+        var history = loadHistory(context)
+        history.add(0, imgPath)
+
+        history = if (history.size > HISTORY_LIST_SIZE)
+            history.subList(0, HISTORY_LIST_SIZE) else history
+
+        sharedPrefs.edit()
+                .putStringSet(context.getString(R.string.history_key), HashSet(history))
+                .apply()
+    }
+}
